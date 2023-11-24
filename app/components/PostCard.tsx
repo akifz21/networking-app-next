45d454d913c7da2468add50b3307ac94f951a4d1
akifz21 <akifz2199@gmail.com>
2023-11-24 14:58:39 +0300

    feat: post card

---
+++ app/components/PostCard.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import Image from "next/image";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+
+type Props = {};
+
+export default function PostCard({}: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>User Name</CardTitle>
+        <CardDescription>
+          {"Fri Nov 24 2023 14:15:46 GMT+0300 (GMT+03:00)"}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="relative">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo eum,
+          cumque maiores eaque consectetur modi error facilis iure veritatis
+          nostrum voluptatibus impedit deserunt qui delectus cupiditate totam
+          quas, dignissimos fugiat ipsa. Quos et vitae quaerat harum sint, enim
+          facere fugit mollitia iste veniam sit quam impedit officiis, ratione
+          illum labore.
+        </p>
+      </CardContent>
+      <CardFooter>
+        <div className="relative h-80 w-full">
+          <div className="grid grid-cols-3 gap-2 h-full">
+            <div className=" relative h-full">
+              <Image src={"/post.jpg"} fill alt="" objectFit="cover" />
+            </div>
+            <div className=" relative h-full">
+              <Image src={"/auth.jpg"} fill alt="" objectFit="cover" />
+            </div>
+            <div className=" relative h-full">
+              <Image src={"/post.jpg"} fill alt="" objectFit="cover" />
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import PostCard from "./components/PostCard";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <div className="grid grid-cols-4 pt-24">
+      <div className="border-x  h-screen col-span-1"></div>
+      <div className="border-x h-screen flex flex-col col-span-2 px-10 justify-start  gap-4">
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
+      <div className="border-x  h-screen col-span-1"></div>
+    </div>
+  );
 }
