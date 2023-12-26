f25cebefa0bd5d429dc8f3313e66fcbe4514794a
akifz21 <akifz2199@gmail.com>
2023-12-26 22:28:26 +0300

    feat: messages socket connection

--- app/(authenticated)/profile/components/message.tsx
+++ app/(authenticated)/profile/components/message.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/app/components/ui/button";
 import { Loader2, MessageCircle, Send } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Sheet,
   SheetContent,
@@ -14,7 +14,8 @@ import { useAuthStore } from "@/app/stores/authStore";
 import { createRoom } from "@/app/api/room";
 import toast from "react-hot-toast";
 import { Card, CardContent, CardHeader, CardTitle } from "@/app/components/ui/card";
-import { useSocket } from "@/app/hooks/useSocket";
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
 
 type Props = {
   id: string;
@@ -23,13 +24,34 @@ type Props = {
 export default function Message({ id }: Props) {
   const user = useAuthStore((state) => state.user);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [roomId, setRoomId] = useState<string | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [client, setClient] = useState<Client>();
+  useEffect(() => {
+    if (roomId) {
+      const socket = new SockJS("http://localhost:8100/ws");
+      const stompClient = new Client({
+        webSocketFactory: () => socket,
+      });
+
+      stompClient.onConnect = (frame) => {
+        console.log("Connected:", frame, roomId);
+        stompClient.subscribe(`/topic/messages/${roomId}`, (message) => {
+          setMessages((prevMessages) => [...prevMessages, JSON.parse(message.body)]);
+        });
+      };
+
+      stompClient.activate();
+      setClient(stompClient);
+    }
+  }, [roomId]);
 
   const handleRoom = async () => {
     try {
       setIsLoading(true);
       const res = await createRoom({ senderUserId: user.id, receiverUserId: id });
       const room = res.data;
-      console.log(room);
+      setRoomId(room?.id);
     } catch (error: any) {
       toast.error(error.message);
     } finally {
@@ -57,7 +79,15 @@ export default function Message({ id }: Props) {
             <div className="flex flex-col w-full gap-4">
               <div className="flex flex-col gap-4 items-center">
                 <Input id="name" className="col-span-3" />
-                <Button className="flex flex-row items-center gap-2">
+                <Button
+                  onClick={() => {
+                    client?.publish({
+                      destination: `/app/message/${roomId}`,
+                      body: JSON.stringify({ sender: user.id, content: "Textddttt" }),
+                    });
+                  }}
+                  className="flex flex-row items-center gap-2"
+                >
                   Send
                   <Send />
                 </Button>
