1b52d28e721336b555bcfaf210f406fd6fc0cbc0
akifzdemir <akifz2199@gmail.com>
2024-04-30 17:13:23 +0300

    feat: add carousel to post images. up: theme and auth pages

--- app/(authenticated)/page.tsx
+++ app/(authenticated)/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import PostDialog from "../components/header/post-share-dialog";
 import PostList from "../components/post-list";
 import useSWR from "swr";
 import { Post } from "../types";
@@ -10,7 +9,6 @@ export default function Home() {
 
   return (
     <div className="border-x h-full flex flex-col pt-24  px-10 md:w-3/4 lg:w-1/2 w-full justify-start  gap-4">
-      <PostDialog />
       <PostList data={data} isLoading={isLoading} error={error} />
     </div>
   );
